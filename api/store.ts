c5ccc241f6d41514bdc8c626b1cd6cfa42dc8403
oladapsy <dapoodedeyi@yahoo.com>
2026-04-24 15:00:38 +0100

    feature: clean up store config

--- api/store.ts
+++ api/store.ts
@@ -1,23 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
 
-// import { configureStore } from "@reduxjs/toolkit";
-// import { userApi } from "./userApi";
+export const store = configureStore({
+  reducer: {
+    // remains reducer and
+  },
+  // middle ware
+});
 
-// export const store = configureStore({
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
 //   reducer: {
 //     [userApi.reducerPath]: userApi.reducer,
 //   },
 //   middleware: (getDefaultMiddleware) =>
 //     getDefaultMiddleware().concat(userApi.middleware),
-// });
-
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
-
-
-import { configureStore } from "@reduxjs/toolkit";
-
-export const store = configureStore({
-    reducer: {
-
-    }
-})
