735c3768215e1be1ef2a3746f20063b4dda83700
oladapsy <dapoodedeyi@yahoo.com>
2026-04-24 15:24:22 +0100

    added middle ware

--- api/store.ts
+++ api/store.ts
@@ -4,14 +4,14 @@ import { userApi } from "@/api/userApi";
 export const store = configureStore({
   reducer: {
     // remains reducer from the config
+    // Add the generated reducer as a specific top-level slice
     [userApi.reducerPath]: userApi.reducer,
   },
-  // middle ware
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(userApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware().concat(userApi.middleware),
