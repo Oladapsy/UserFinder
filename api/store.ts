cc392b0e47bf258b39d1184759e075318024053d
oladapsy <dapoodedeyi@yahoo.com>
2026-04-24 14:55:44 +0100

    feature: created a store and want to wrap layout with provider

---
+++ api/store.ts
@@ -0,0 +1,23 @@
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import { userApi } from "./userApi";
+
+// export const store = configureStore({
+//   reducer: {
+//     [userApi.reducerPath]: userApi.reducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware().concat(userApi.middleware),
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
+
+
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer: {
+
+    }
+})
